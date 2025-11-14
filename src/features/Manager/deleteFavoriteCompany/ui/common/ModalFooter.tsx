c24396b0067ea946382165c3efd364fe2ec407d1
홍범선 <qjatjs123123@naver.com>
2025-11-14 21:51:32 +0900

    feat: 관심기업 삭제 모달에서 취소 시 모달 닫히는 기능 구현

--- src/features/Manager/deleteFavoriteCompany/ui/common/ModalFooter.tsx
+++ src/features/Manager/deleteFavoriteCompany/ui/common/ModalFooter.tsx
@@ -1,8 +1,11 @@
 import { Button } from "@/shared/ui/Button/Button";
 import { useDeleteFavoriteCompany } from "../../model/useDeleteFavoriteCompany";
+import { useOverlay } from "@/shared/model/useOverlay";
 
 export function ModalFooter() {
   const mutation = useDeleteFavoriteCompany();
+  const { close } = useOverlay();
+
   return (
     <div className="flex flex-col w-full gap-2 mt-[10px]">
       <Button
@@ -12,7 +15,11 @@ export function ModalFooter() {
       >
         삭제
       </Button>
-      <Button style="outline" className="justify-center">
+      <Button
+        style="outline"
+        className="justify-center"
+        onClick={() => close()}
+      >
         취소
       </Button>
     </div>
