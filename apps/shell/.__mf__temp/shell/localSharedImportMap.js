bdbb8970ddc19e2dafe9d639f2450f4fb7e5da6b
홍범선 <qjatjs123123@naver.com>
2025-12-02 05:21:06 +0900

    1

--- apps/shell/.__mf__temp/shell/localSharedImportMap.js
+++ apps/shell/.__mf__temp/shell/localSharedImportMap.js
@@ -8,6 +8,11 @@
           let pkg = await import("__mf__virtual/shell__prebuild__react__prebuild__.js");
             return pkg;
         }
+      ,
+        "react-dom": async () => {
+          let pkg = await import("__mf__virtual/shell__prebuild__react_mf_2_dom__prebuild__.js");
+            return pkg;
+        }
       
     }
       const usedShared = {
@@ -24,6 +29,36 @@
               }
               usedShared["react"].loaded = true
               const {"react": pkgDynamicImport} = importMap
+              const res = await pkgDynamicImport()
+              const exportModule = {...res}
+              // All npm packages pre-built by vite will be converted to esm
+              Object.defineProperty(exportModule, "__esModule", {
+                value: true,
+                enumerable: false
+              })
+              return function () {
+                return exportModule
+              }
+            },
+            shareConfig: {
+              singleton: false,
+              requiredVersion: "^19.2.0",
+              
+            }
+          }
+        ,
+          "react-dom": {
+            name: "react-dom",
+            version: "19.2.0",
+            scope: ["default"],
+            loaded: false,
+            from: "shell",
+            async get () {
+              if (false) {
+                throw new Error(`Shared module '${"react-dom"}' must be provided by host`);
+              }
+              usedShared["react-dom"].loaded = true
+              const {"react-dom": pkgDynamicImport} = importMap
               const res = await pkgDynamicImport()
               const exportModule = {...res}
               // All npm packages pre-built by vite will be converted to esm
